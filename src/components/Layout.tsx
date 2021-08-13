6146152331f18e4e14b1d5725d55472997dbcb06
PauloDev13 <devpgm2020@gmail.com>
2021-08-12 23:27:26 -0300

    Adicionando Style ao componente Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,30 @@
 import Head from 'next/head';
 import {AppBar, Container, Toolbar, Typography} from '@material-ui/core';
+import useStyles from '../../utils/styles';
 
 interface LayoutProps {
   children?: any;
 }
 
 function Layout(props: LayoutProps) {
+  const classes = useStyles();
+
   return (
     <div>
       <Head>
         <title>Next Amazonas</title>
       </Head>
-      <AppBar position={'static'}>
+      <AppBar className={classes.navbar} position={'static'}>
         <Toolbar>
           <Typography>amazonas</Typography>
         </Toolbar>
       </AppBar>
-      <Container>
+      <Container className={classes.main}>
         {props.children}
       </Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Next Amazona</Typography>
+      </footer>
     </div>
   );
 };
